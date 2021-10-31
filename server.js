4bcc75857d9f78d868aecb7829bafd7a75e4ceee
sandul jayathileka <sanduljayathileka@yahoo.com>
2021-10-31 16:23:07 +0530

    changed get

--- server.js
+++ server.js
@@ -150,12 +150,20 @@ app.post("/login", (req, res) => {
 //   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 // });
 
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
+
 
 app.listen(PORT, () => {
   console.log("Server listening on " + PORT);
