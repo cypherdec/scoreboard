d7b77b0803d0d6bac13a1979e246c23479e2ed14
sandul jayathileka <sanduljayathileka@yahoo.com>
2021-10-31 15:00:23 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,145 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3001;
+
+app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/scoreboardDB");
+
+const scoreSchema = mongoose.Schema({
+  team: String,
+  teamScore: String,
+  position: String,
+});
+
+const userSchema = mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const Score = mongoose.model("Score", scoreSchema);
+
+const User = mongoose.model("User", userSchema);
+
+const default1 = new Score({
+  team: "Level 1",
+  teamScore: 0,
+  position: 0,
+});
+
+const default2 = new Score({
+  team: "Level 2",
+  teamScore: 0,
+  position: 0,
+});
+
+const default3 = new Score({
+  team: "Level 3",
+  teamScore: 0,
+  position: 0,
+});
+
+const default4 = new Score({
+  team: "Level 4",
+  teamScore: 0,
+  position: 0,
+});
+
+const defaultTeams = [default1, default2, default3, default4];
+
+app.get("/scores", (req, res) => {
+  Score.find(function (err, scores) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (scores.length === 0) {
+        Score.insertMany(defaultTeams, function (err) {
+          if (err) {
+            console.log(err);
+          }
+        });
+      }
+      res.json(scores);
+    }
+  });
+});
+
+app.post("/update", (req, res) => {
+  let data = req.body.scoreData;
+
+  data.forEach((score) => {
+    saveScore = new Score();
+    saveScore = score;
+
+    Score.findOneAndUpdate({ _id: score._id }, score, { multi: true })
+      .then((updatedRows) => {
+        res.send("updated successfully");
+      })
+      .catch((err) => {
+        res.send(err);
+        console.log(err);
+      });
+  });
+
+  console.log(data);
+});
+
+app.get("/login", (req, res) => {
+  User.find((err, users) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (users.length === 0) {
+        res.send("password");
+      } else {
+        res.send("hidden");
+      }
+    }
+  });
+});
+
+app.post("/newlogin", (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  const newUser = new User({
+    username: username,
+    password: password,
+  });
+
+  if (newUser.save()) {
+    res.send("ok");
+  } else {
+    res.send("err");
+  }
+});
+
+app.post("/login", (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  User.findOne({ username: username, password: password }, function (err, obj) {
+    if(err){
+        console.log(err);
+    }
+    else if (obj){
+        res.send('ok');
+    }
+    else{
+        res.send('err');
+    }
+  });
+
+});
+
+app.listen(PORT, () => {
+  console.log("Server listening on " + PORT);
+});
