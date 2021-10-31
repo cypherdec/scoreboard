a40d130d83d9fa357f26fed83028a2779aea2877
sandul jayathileka <sanduljayathileka@yahoo.com>
2021-10-31 16:31:06 +0530

    fixed get

--- server.js
+++ server.js
@@ -157,12 +157,13 @@ app.post("/login", (req, res) => {
 //   });
 // }
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build')); // serve the static react app
+  app.get(/^\/(?!api).*/, (req, res) => { // don't serve api routes to react app
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
   });
-}
+  console.log('Serving React App...');
+};
 
 
 app.listen(PORT, () => {
