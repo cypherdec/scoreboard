e5795b80fa00e4de3cb8d0a4a54475e489c72911
sandul jayathileka <sanduljayathileka@yahoo.com>
2021-10-31 17:09:45 +0530

    test

--- server.js
+++ server.js
@@ -168,13 +168,14 @@ app.post("/login", (req, res) => {
 // "start": "node server.js",
 //     "heroku-postbuild": "cd client && npm install --only=dev && npm install && npm run build"
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-  app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 }
+app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 
 
 
