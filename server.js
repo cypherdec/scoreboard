67a5282e22139aa096deee12d1ed708576a2538a
sandul jayathileka <sanduljayathileka@yahoo.com>
2021-10-31 16:47:44 +0530

    fixed gitignore

--- server.js
+++ server.js
@@ -146,9 +146,9 @@ app.post("/login", (req, res) => {
 });
 
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static("client/build"));
@@ -157,13 +157,13 @@ app.post("/login", (req, res) => {
 //   });
 // }
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build')); // serve the static react app
-  app.get(/^\/(?!api).*/, (req, res) => { // don't serve api routes to react app
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
-  });
-  console.log('Serving React App...');
-};
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build')); // serve the static react app
+//   app.get(/^\/(?!api).*/, (req, res) => { // don't serve api routes to react app
+//     res.sendFile(path.join(__dirname, './client/build/index.html'));
+//   });
+//   console.log('Serving React App...');
+// };
 
 
 app.listen(PORT, () => {
