13c8e92e14ce3f1980d8f85451b077e8044f10df
sandul jayathileka <sanduljayathileka@yahoo.com>
2021-11-01 10:28:24 +0530

    fixed app.use client

--- server.js
+++ server.js
@@ -13,9 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.set("view engine", "ejs");
-app.use(express.static("public"));
-
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static( "public"));
+app.use('*', express.static(path.join(__dirname, "client", "build")));
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
