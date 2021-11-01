f37164d0bc2a6c8f67f95302f18ede27eababd63
sandul jayathileka <sanduljayathileka@yahoo.com>
2021-11-01 15:04:43 +0530

    route

--- server.js
+++ server.js
@@ -150,9 +150,9 @@ app.post("/login", (req, res) => {
 
 
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname + '/client/build/index.html'))
-})
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname + '/client/build/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log("Server listening on " + PORT);
